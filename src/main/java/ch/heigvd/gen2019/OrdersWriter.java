1b16a325f6ac326c10ba33f3d466134bb34fd9de
Nohan Budry <nohan.budry@icloud.com>
2019-05-10 14:42:48 +0200

    Replace method with method object, OrdersWriter.getOrderInfo(), Long Method

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -11,7 +11,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            sb.append(getOrderInfo(orders.getOrder(i)));
+            sb.append(orders.getOrder(i));
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -19,29 +19,5 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
-    }
-
-    private String getOrderInfo(Order order) {
-
-        StringBuffer sb = new StringBuffer();
-
-        sb.append("{");
-        sb.append("\"id\": ");
-        sb.append(order.getOrderId());
-        sb.append(", ");
-        sb.append("\"products\": [");
-
-        for (int j = 0; j < order.getProductsCount(); j++) {
-            sb.append(order.getProduct(j));
-        }
-
-        if (order.getProductsCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        sb.append("]");
-        sb.append("}, ");
-
-        return sb.toString();
     }
 }
